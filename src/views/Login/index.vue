b241dc369d8bad1453a888e20a13cf7ad6cf799c
maliang <mal@bettem.com>
2020-03-11 19:53:14 +0800

    构建项目、配置项目

---
+++ src/views/Login/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div id="login">
+    登陆页
+    <el-button>默认按钮</el-button>
+    <el-button type="primary">主要按钮</el-button>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style lang="scss" scoped>
+#login {
+  background-color: #344a5f;
+  height: 100vh;
+}
+</style>
